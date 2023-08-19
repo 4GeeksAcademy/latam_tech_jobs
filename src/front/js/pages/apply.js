30d5c6a9efdadc6c308c368e0d905f9761b825c4
Argelio Baca <49691196+Argeliosk8@users.noreply.github.com>
2023-08-19 01:55:07 +0000

    created the apply view

--- src/front/js/pages/apply.js
+++ src/front/js/pages/apply.js
@@ -1,5 +1,59 @@
 import React from "react";
 
 export function Apply(){
-    return <h1>Hoy soy el apply form</h1>
+    return (
+        <div className="container-md d-flex flex-column justify-content-center border border-dark-subtle p-4 mt-3 bg-light rounded">
+            <div class="row g-3 justify-content-around mb-3">
+                <div class="col-3">
+                    <label for="inputPassword6" class="col-form-label"><i class="fa-solid fa-suitcase"></i> Resume/CV</label>
+                </div>
+                <div class="col-6">
+                    <input type="file" class="form-control" aria-label="file example" required/>
+                </div>
+            </div>
+            <div class="row g-3 justify-content-around  mb-3">
+                <div class="col-3">
+                    <label for="nameInput" class="col-form-label"><i class="fa-solid fa-user"></i> Full name</label>
+                </div>
+                <div class="col-6">
+                    <input type="email" class="form-control" id="nameInput" placeholder="John Doe" required/>
+                </div>
+            </div>
+            <div class="row g-3 justify-content-around mb-3">
+                <div class="col-3">
+                    <label for="emailInput" class="col-form-label"><i class="fa-solid fa-envelope"></i> Email</label>
+                </div>
+                <div class="col-6">
+                    <input type="text" class="form-control" id="emailInput" placeholder="name@email.com" required/>
+                </div>
+            </div>
+            <div class="row g-3 justify-content-around mb-3">
+                <div class="col-3">
+                    <label for="phoneInput" class="col-form-label"><i class="fa-solid fa-phone"></i> Phone</label>
+                </div>
+                <div class="col-6">
+                    <input type="text" class="form-control" id="phoneInput" placeholder="000-000-0000" required/>
+                </div> 
+            </div>   
+            <div class="row g-3 justify-content-around mb-3">
+                <div class="col-3">
+                    <label for="companyInput" class="col-form-label"><i class="fa-solid fa-building"></i> Current Company</label>
+                </div>
+                <div class="col-6">
+                    <input type="text" class="form-control" id="companyInput" placeholder="Company name" required/>
+                </div>  
+            </div>  
+            <div class="row g-3 justify-content-around mb-3">
+                <div class="col-9">
+                    <label for="exampleFormControlTextarea1" class="form-label"><i class="fa-solid fa-message"></i> Additional Comments</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                </div>  
+            </div>  
+            <div class="row g-3 justify-content-around mb-3">
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>  
+            </div>  
+        </div>
+    )
 }
--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -9,32 +9,34 @@ export function SignUp(){
             <div className="d-flex flex-column col-6 p-4">
                 <div class="mb-3">
                     <label for="inputCompanyName" class="form-label"><i class="fa-regular fa-building"></i> Company Name</label>
-                    <input type="text" class="form-control" id="inputCompanyName"/>
+                    <input type="text" class="form-control" id="inputCompanyName" required/>
+                    <div class="invalid-feedback">Example invalid form file feedback</div>
                 </div>
                 <div class="mb-3">
                     <label for="inputFullName" class="form-label"><i class="fa-solid fa-user"></i> Full Name</label>
-                    <input type="text" class="form-control" id="inputFullName"/>
+                    <input type="text" class="form-control" id="inputFullName" required/>
                 </div>
                 <div class="mb-3">
                     <label for="inputEmail" class="form-label"><i class="fa-solid fa-envelope"></i> Email</label>
-                    <input type="text" class="form-control" id="inputEmail"/>
+                    <input type="text" class="form-control" id="inputEmail" required/>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword" class="form-label"><i class="fa-solid fa-key"></i> Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword"/>
+                    <input type="password" class="form-control" id="exampleInputPassword" required/>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword2" class="form-label"><i class="fa-solid fa-key"></i> Repeat Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword2"/>
+                    <input type="password" class="form-control" id="exampleInputPassword2" required/>
                 </div>
             </div>
             <div className="d-flex flex-column col-6 p-4">
                 <div class="mb-3">
                     <label for="inputPhone" class="form-label"><i class="fa-solid fa-phone"></i> Telephone</label>
-                    <input type="text" class="form-control" id="inputPhone"/>
+                    <input type="text" class="form-control" id="inputPhone" required/>
                 </div>
                 <div className="mb-3">
-                    <select class="form-select" aria-label="Default select example">
+                    <label for="inputCountry" class="form-label"><i class="fa-solid fa-earth-americas"></i> Country</label>
+                    <select class="form-select" aria-label="Default select example" required>
                         <option selected>Select your country</option>
                         {
                             countries.map((country, id)=>(
@@ -45,7 +47,7 @@ export function SignUp(){
                 </div>
                 <div class="mb-3">
                     <label for="inputAddress" class="form-label"><i class="fa-solid fa-location-dot"></i> Address</label>
-                    <input type="text" class="form-control" id="inputAddress"/>
+                    <input type="text" class="form-control" id="inputAddress" required/>
                 </div>
                 <div class="mb-3">
                     <label for="inputWebsite" class="form-label"><i class="fa-solid fa-cloud"></i> Website</label>
@@ -59,7 +61,7 @@ export function SignUp(){
         </div>
         <div class="d-flex flex-column">
                 <span class="input-group-text">Company Description</span>
-                <textarea class="form-control" aria-label="With textarea"></textarea>
+                <textarea class="form-control" aria-label="With textarea" rows="5" required></textarea>
         </div>
         <div className="d-flex justify-content-center mt-4">
             <button type="submit" class="btn btn-primary">Submit</button>
