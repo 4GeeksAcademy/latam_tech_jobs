4c638ed2de259b52329d15862d2a679337b9e63b
Argelio Baca <49691196+Argeliosk8@users.noreply.github.com>
2023-08-22 01:55:02 +0000

    company user model created + user registration endpoint created using bcrypt

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    company_name = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(80), unique=False, nullable=False)
+    company_description = db.Column(db.String(1000), unique=False, nullable=False)
+    phone = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    country = db.Column(db.String(120), unique=False, nullable=False)
+    address = db.Column(db.String(120), unique=False, nullable=False)
+    website = db.Column(db.String(120), unique=False, nullable=False)
+    linkedin = db.Column(db.String(120), unique=False, nullable=False)
+    creation_date = db.Column(db.DateTime)
 
     def __repr__(self):
         return f'<User {self.email}>'
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_bcrypt import Bcrypt
 
 api = Blueprint('api', __name__)
-
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -16,3 +18,24 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/register', methods=['POST'])
+def register():
+    company_name = request.json.get("company_name", None)
+    password = request.json.get("password", None)
+    company_description = request.json.get("company_description", None)
+    phone = request.json.get("phone", None)
+    email = request.json.get("email", None)
+    country = request.json.get("country", None)
+    address = request.json.get("address", None)
+    website = request.json.get("website", None)
+    linkedin = request.json.get("linkedin", None)    
+
+    if email and password: 
+        pw_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(company_name = company_name, password_hash = pw_hash, company_description = company_description, phone = phone, email = email, country = country, address = address, website = website, linkedin = linkedin)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': "user registered successfully"}), 201
+    else:
+        return jsonify({'messge': "error"})
