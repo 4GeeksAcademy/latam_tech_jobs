a28a69928176eba2ef643cc38c335ae0f64c79ec
Juan Goyeneche <80859255+JuanGoye15@users.noreply.github.com>
2023-08-29 01:37:24 +0000

    avances

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,30 +1,27 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Context } from "../store/appContext";
-import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
-	const {actions} = useContext(Context)
-	const navigate = useNavigate()
-	const handleLogoutClick = (e)=>{
-		e.preventDefault()
-		actions.logout()
-		alert("you were loged out successfully")
-		navigate('/')
-		window.location.reload(false);
-	}
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button onClick={handleLogoutClick} className="btn btn-primary">Sign out</button>
-					</Link>
+		<>
+			<nav className="navbar navbar-light bg-light mb-3 container-fluid">
+				<div className="float-left p-2">
+					<h1><a href="/" className="text-decoration-none"><Link to={'/'}></Link>Latam Tech Jobs</a></h1>
+				</div>				
+				<div className="p-2">
 				</div>
-			</div>
-		</nav>
+				<div className="row float-right text-decoration-none">
+					<ul className="col-md nav navbar-nav navbar-right text-decoration-none float-left"  style={{paddingTop: 20}}>
+						<li><a href="#" className="text-decoration-none"><i class="fa-solid fa-right-to-bracket"></i><Link to={'/login'} className="text-decoration-none">Login</Link></a></li>
+					</ul>
+					<ul className="col-md nav navbar-nav navbar-right"  style={{paddingTop: 20}}>
+						<li><a href="#" className="text-decoration-none"><i class="fa-solid fa-user-plus"></i><Link to={'/signup'} className="text-decoration-none">Sing-up</Link></a></li>
+					</ul>
+					<ul className="col-md nav navbar-nav navbar-right p-2" style={{width: 180}}>
+						<button type="button" className="btn btn-primary text-decoration-none" style={{height: 40}}><Link to={'/post'} className="text-decoration-none"><p className="text-white">Post a job</p></Link></button>
+					</ul>
+				</div>
+			</nav>
+		</>
 	);
 };
